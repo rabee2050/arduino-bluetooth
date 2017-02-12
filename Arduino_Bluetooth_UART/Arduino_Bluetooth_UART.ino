1468dedf5ecdd6598e4de4cf2ced3403eccccfe3
Rabee Alhattawi <rabee2050@gmail.com>
2017-02-12 20:33:31 +0300

    Update Arduino_Bluetooth_UART.ino

--- Arduino_Bluetooth_UART/Arduino_Bluetooth_UART.ino
+++ Arduino_Bluetooth_UART/Arduino_Bluetooth_UART.ino
@@ -4,40 +4,42 @@
   info@tatco.cc
 
   Note:
-  Make sure the baud rate of the bluetooth module is 9600bps.
+  Make sure the baud rate of the bluetooth is 9600, and it comes
+  by default if it is new kit.
 
   Connection:
   arduino_rx_pin  ------->   Bluetooth_tx_pin
   arduino_tx_pin  ------->   Bluetooth_rx_pin
 
-  Pins that can be used as rx on Mega and Mega 2560:
-  10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
-  Pins that can be used as rx on on Leonardo:
-  8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
-  If you are using Bluefruit module then make sure to connect CTS pin to ground.
 
 */
-#define arduino_rx_pin 10  
-#define arduino_tx_pin 11
+
 
 #include <SoftwareSerial.h>
+#include <Servo.h>
+
+#define arduino_rx_pin 10  //must be inturrpt pin
+#define arduino_tx_pin 11
+#define lcd_size 3 //this will define number of LCD on the phone app
+#define refresh_time  5 //the data will be updated on the app every 5 seconds.
 
 SoftwareSerial mySerial(arduino_rx_pin, arduino_tx_pin); // RX, TX
 
 char mode_action[54];
 int mode_val[54];
 String mode_feedback;
+String lcd[lcd_size];
+unsigned long last = millis();
+Servo myServo[54];
 
 void setup(void)
 {
   Serial.begin(9600);
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for native USB port only
-//  }
+  //  while (!Serial) {
+  //    ; // wait for serial port to connect. Needed for native USB port only
+  //  }
   Serial.println("Goodnight moon!");
-  
+
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);//you have to change this if you change bluetooth baudrate.
   //mySerial.println("Hello, world?");
@@ -48,56 +50,50 @@ void setup(void)
       mode_action[i] = 'x';
       mode_val[i] = 'x';
     }
-    
     else {
       mode_action[i] = 'o';
       mode_val[i] = 0;
       pinMode(i, OUTPUT);
     }
   }
-  }
+}
 #endif
 
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
-  for (byte i = 0; i <= 13; i++) {
-    if (i == 0 || i == 1 || i == arduino_rx_pin || i == arduino_tx_pin ) {
-      mode_action[i] = 'x';
-      mode_val[i] = 'x';
-    }
-    
-    else {
-      mode_action[i] = 'o';
-      mode_val[i] = 0;
-      pinMode(i, OUTPUT);
-    }
+for (byte i = 0; i <= 13; i++) {
+  if (i == 0 || i == 1 || i == arduino_rx_pin || i == arduino_tx_pin ) {
+    mode_action[i] = 'x';
+    mode_val[i] = 'x';
   }
+  else {
+    mode_action[i] = 'o';
+    mode_val[i] = 0;
+    pinMode(i, OUTPUT);
+  }
+}
 #endif
-
-
 }
 
-void update_input() {
-  for (int i = 0; i < sizeof(mode_action); i++) {
-    if (mode_action[i] == 'i') {
-      mode_val[i] = digitalRead(i);
 
-    }
-  }
-}
 
 void loop(void)
 {
-
-  if (mySerial.available()) {
-    update_input();
+  lcd[0] = "Test 1 LCD";// you can send any data to your mobile phone.
+  lcd[1] = "Test 2 LCD";// you can send any data to your mobile phone.
+  lcd[2] = analogRead(1);//  send analog value of A1
+ 
+  if ( mySerial.available() )
+  {
     process();
   }
-
+  update_input();
+  update_app();//will update all values in the mobile app.
 }
 
 void process() {
 
   String command = mySerial.readStringUntil('/');
+
   if (command == "digital") {
     digitalCommand();
   }
@@ -108,6 +104,10 @@ void process() {
 
   if (command == "mode") {
     modeCommand();
+  }
+
+  if (command == "servo") {
+    servo();
   }
 
   if (command == "allonoff") {
@@ -141,61 +141,84 @@ void analogCommand() {
 
 }
 
+void servo() {
+  int pin, value;
+  pin = mySerial.parseInt();
+  if (mySerial.read() == '/') {
+    value = mySerial.parseInt();
+    myServo[pin].write(value);
+    mode_val[pin] = value;
+  }
+}
 
 void modeCommand() {
   int pin;
   pin = mySerial.parseInt();
   mode_feedback = "";
-  String mode = mySerial.readStringUntil(' ');
-  if (mode == "/input") {
-    pinMode(pin, INPUT);
-    mode_action[pin] = 'i';
-    mode_feedback += "D";
-    mode_feedback += pin;
-    mode_feedback += " set as INPUT!";
+  if (mySerial.read() == '/') {
+
+    String mode = mySerial.readStringUntil('\r');
+    if (mode == "input") {
+      pinMode(pin, INPUT);
+      mode_action[pin] = 'i';
+      mode_feedback += "D";
+      mode_feedback += pin;
+      mode_feedback += " set as INPUT!";
+    }
+
+    if (mode == "output") {
+      pinMode(pin, OUTPUT);
+      mode_action[pin] = 'o';
+      mode_feedback += "D";
+      mode_feedback += pin;
+      mode_feedback += " set as OUTPUT!";
+    }
+
+    if (mode == "pwm") {
+      pinMode(pin, OUTPUT);
+      mode_action[pin] = 'p';
+      mode_feedback += "D";
+      mode_feedback += pin;
+      mode_feedback += " set as PWM!";
+    }
+
+    if (mode == "servo") {
+      myServo[pin].attach(pin);
+      mode_action[pin] = 's';
+      mode_feedback += "D";
+      mode_feedback += pin;
+      mode_feedback += " set as SERVO!";
+    }
+    allstatus();
   }
-
-  if (mode == "/output") {
-    pinMode(pin, OUTPUT);
-    mode_action[pin] = 'o';
-    mode_feedback += "D";
-    mode_feedback += pin;
-    mode_feedback += " set as OUTPUT!";
-  }
-
-  if (mode == "/pwm") {
-    pinMode(pin, OUTPUT);
-    mode_action[pin] = 'p';
-    mode_feedback += "D";
-    mode_feedback += pin;
-    mode_feedback += " set as PWM!";
-  }
-
-  allstatus();
-
 }
 
 void allonoff() {
   int pin, value;
   value = mySerial.parseInt();
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)
-  for (byte i = 0; i <= 13; i++) {
+  for (byte i = 0; i < sizeof(mode_action); i++) {
     if (mode_action[i] == 'o') {
       digitalWrite(i, value);
       mode_val[i] = value;
     }
   }
-#endif
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-  for (byte i = 0; i <= 53; i++) {
-    if (mode_action[i] == 'o') {
-      digitalWrite(i, value);
-      mode_val[i] = value;
-    }
-  }
-#endif
-allstatus();
+}
 
+void update_input() {
+  for (int i = 0; i < sizeof(mode_action); i++) {
+    if (mode_action[i] == 'i') {
+      mode_val[i] = digitalRead(i);
+
+    }
+  }
+}
+
+void update_app() {
+  int refreshVal=refresh_time*1000;
+  if (millis() - last > refreshVal) {
+    allstatus();
+    last = millis();
+  }
 }
 
 
@@ -208,17 +231,17 @@ void allstatus() {
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)//Mega
   for (byte i = 0; i <= 53; i++) {
     data_status += "\"";
-      data_status += mode_action[i];
-      data_status += "\"";
-      if(i!=53)data_status += ",";
+    data_status += mode_action[i];
+    data_status += "\"";
+    if (i != 53)data_status += ",";
   }
 #endif
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)//Leo
   for (byte i = 0; i <= 13; i++) {
     data_status += "\"";
-      data_status += mode_action[i];
-      data_status += "\"";
-      if(i!=13)data_status += ",";
+    data_status += mode_action[i];
+    data_status += "\"";
+    if (i != 13)data_status += ",";
   }
 #endif
   data_status += "],";
@@ -227,13 +250,13 @@ void allstatus() {
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)//Mega
   for (byte i = 0; i <= 53; i++) {
     data_status += mode_val[i];
-    if(i!=53)data_status += ",";
+    if (i != 53)data_status += ",";
   }
 #endif
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)//Leo
   for (byte i = 0; i <= 13; i++) {
     data_status += mode_val[i];
-    if(i!=13)data_status += ",";
+    if (i != 13)data_status += ",";
   }
 #endif
   data_status += "],";
@@ -241,29 +264,40 @@ void allstatus() {
   data_status += "\"analog\":[";
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)//Mega
   for (byte i = 0; i <= 15; i++) {
-        data_status += analogRead(i);
-    if(i!=15)data_status += ",";
+    data_status += analogRead(i);
+    if (i != 15)data_status += ",";
   }
 #endif
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega16U4__)//Leo
   for (byte i = 0; i <= 5; i++) {
     data_status += analogRead(i);
-    if(i!=5)data_status += ",";
+    if (i != 5)data_status += ",";
   }
 #endif
   data_status += "],";
+
+  data_status += "\"lcd\":[";
+  for (byte i = 0; i <= lcd_size-1; i++) {
+    data_status += "\"";
+    data_status += lcd[i];
+    data_status += "\"";
+    if (i != lcd_size-1)data_status += ",";
+  }
+  data_status += "],";
+
   data_status += "\"mode_feedback\":\"";
   data_status += mode_feedback;
   data_status += "\",";
+  
   data_status += "\"boardname\":\"";
-  #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)//Mega
-  data_status +="kit_mega";
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)//Mega
+  data_status += "kit_mega";
 #endif
 #if defined(__AVR_ATmega32U4__)//Leo
-  data_status +="kit_leo";
+  data_status += "kit_leo";
 #endif
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega16U4__)//UNO
-  data_status +="kit_uno";
+  data_status += "kit_uno";
 #endif
   data_status += "\",\"boardstatus\":1";
   data_status += "}";
